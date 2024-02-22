dfedb467a97d1a776b11378918b84d47e7ddf73d
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2024-02-22 13:21:31 +0100

    Tema 2.1 ML terminado

---
+++ Prácticas/2º Curso/Señales y Sistemas/Práctica 2/practica_2.m
@@ -0,0 +1,38 @@
+close all
+
+x = [1 2 -2];
+h = [1 3 0 1 2 1 2];
+
+y = conv(x, h, 'full')
+
+figure(1)
+stem(y, 'filled', "LineWidth", 1.5, "MarkerSize", 4);
+xlabel('n');
+ylabel('y[n]');
+title('Convolución discreta de x[n] y h[n]')
+
+y1 = convn(x,h)
+
+y2 = conv2(x, h)
+
+x = [1 -1]
+
+y=
+
+
+
+% Obtener todas las figuras abiertas:
+figs = get(0, 'children');
+
+
+% Recorrer todas las figuras:
+for i = 1:length(figs)
+    % Seleccionar la figura actual:
+    figure(figs(i));
+
+    % Crear el nombre del archivo:
+    filename = sprintf('Figura%d.png', i);
+
+    % Guardar la figura en un archivo PNG:
+    print(filename, '-dpng');
+end
