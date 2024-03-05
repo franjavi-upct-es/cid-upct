26a924fdbd73b14cb505d305f835995dd517b009
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2024-03-05 10:25:18 +0100

    Nuevos ejercicios en clase y empezada la memoria de la práctica 2 SS

--- Prácticas/2º Curso/Señales y Sistemas/Práctica 2/conv1.m
+++ Prácticas/2º Curso/Señales y Sistemas/Práctica 2/conv1.m
@@ -1,9 +1,15 @@
 function y = conv1(x, h)
+    % N es la longitud de la secuencia x
     N = length(x);
+    % M es la longitud de la secuencia h
     M = length(h);
+    % Inicializa la secuencia de salida y con ceros
     y = zeros(1, N+M-1);
+    % Itera sobre cada elemento de la secuencia de salida
     for n = 1:N+M-1
+        % Itera sobre los elementos de x y h que se superponen para el elemento actual de y
         for k = max(1, n+1-M):min(n, N)
+            % Suma el producto de los elementos correspondientes de x y h a la secuencia de salida
             y(n) = y(n) + x(k) * h(n-k+1);
         end
     end
--- Prácticas/2º Curso/Señales y Sistemas/Práctica 2/practica_2.m
+++ Prácticas/2º Curso/Señales y Sistemas/Práctica 2/practica_2.m
@@ -14,15 +14,31 @@ title('Convolución discreta de x[n] y h[n]')
 
 y1 = conv1(x,h);
 
-y2 = conv2(x, h);
 
-disp(all(y == y1));
-disp(all(y == y2));
+figure(2)
+stem(y1, "filled", "LineWidth", 1.5, "MarkerSize", 4);
+xlabel('n');
+ylabel("y[n]")
+title('Conv1 y[n]')
+
+y2 = conv2d(x, h);
+
+figure(3)
+stem(y2, "filled", "LineWidth", 1.5, "MarkerSize", 4);
+xlabel('n');
+ylabel("y[n]")
+title('Conv2 y[n]')
+
+% Las siguiente funciones recorren todos los elementos de las señales y
+% comprueban si sus valores coinciden con valores bnarios (1 sí, 0 no).
+disp(all(y == y1)); % 1
+disp(all(y == y2)); % 1
+disp(all(y1 == y2)); % 1
 
 
 % Obtener todas las figuras abiertas:
 figs = get(0, 'children');
-
+figs = flip(figs);
 
 % Recorrer todas las figuras:
 for i = 1:length(figs)
@@ -35,3 +51,5 @@ for i = 1:length(figs)
     % Guardar la figura en un archivo PNG:
     print(filename, '-dpng');
 end
+
+
