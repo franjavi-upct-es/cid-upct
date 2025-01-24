594b9695f4af9dff0fc49947ad83d8b289d276fe
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2025-01-24 11:20:46 +0100

    Prácticas de SS eliminadas y Nueva carpeta de cursos de ML

--- Prácticas/2º Curso/2º Cuatrimestre/Señales y Sistemas/Práctica 3/itfourier.m
+++
@@ -1,7 +0,0 @@
-function [x,t] = itfourier(X,w,dw,tmax)
-    % Aplicar la Transformada de Fourier a la señal en el dominio de la frecuencia
-    [x_tilde, t] = tfourier(X,w,dw,tmax);
-
-    % Escalar la señal y tomar la parte real para obtener la señal en el dominio del tiempo
-    x = real(dw/(2*pi) * x_tilde);
-end
