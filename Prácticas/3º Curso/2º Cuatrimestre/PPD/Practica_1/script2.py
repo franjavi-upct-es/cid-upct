31f914acc159eb9087e6fdd8601fe621abe6d6ab
Francisco Javier Mercader Martínez <franciscojavier.mercader@edu.upct.es>
2026-06-07 21:03:51 +0200

    PPD y RD aprobados

--- Prácticas/3º Curso/2º Cuatrimestre/PPD/Practica_1/script2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print(f'hello from script 2, executed by process {os.getpid()}.')
-f= open("file2.txt","w+")
-f.write("hello from script 2")
-f.close()
--- Prácticas/3º Curso/2º Cuatrimestre/PPD/Practica_1/script3.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print(f'hello from script 3, executed by process {os.getpid()}.')
-f= open("file3.txt","w+")
-f.write("hello from script 3")
-f.close()
